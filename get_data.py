cdde2a119c7ea91fb153a40ee9ffd45950afb48c
Daniel San Martin <dsanmartinreyes@gmail.com>
2018-04-22 00:02:42 -0300

    Weather data

---
+++ get_data.py
@@ -0,0 +1,107 @@
+import numpy as np
+import time
+import requests
+import matplotlib.pyplot as plt
+from matplotlib import cm
+#from WunderWeather import weather
+from scipy import interpolate
+
+#extractor = weather.Extract('35c89267da3bc202')
+#[location,current] = extractor.features("-38.7290843,-72.6378264",(('geolookup',''),('now','')))
+#print("Current Temperature in %s is: %s" %(location.data.city, current.temp_c))
+
+base = "http://api.wunderground.com/api/35c89267da3bc202/conditions/q/"
+
+def getWeather(start_lat, start_lng, end_lat, end_lng, M, N):
+  delta_lat = (end_lat - start_lat) / M
+  delta_lng = (end_lng - start_lng) / N
+  
+  temperature = np.zeros((M, N))
+  wind_speed = np.zeros((M, N))
+  wind_direction = np.zeros((M, N))
+  relative_humidity = np.zeros((M, N))
+  pressure = np.zeros((M, N))
+  
+  
+  for i in range(M):
+    for j in range(N):
+      lat = start_lat + i * delta_lat
+      lng = start_lng + j * delta_lng
+            
+      #_, current = extractor.features(str(lat) + "," + str(lng),(('geolookup',''),('now','')))
+
+      url = base + str(lat) + "," + str(lng) + ".json" 
+      data = requests.get(url).json()['current_observation']
+
+      t = float(data['temp_c'])
+      ws = float(data['wind_kph'])
+      wd = float(data['wind_degrees'])
+      rh = float(data['relative_humidity'][:-1])
+      p = float(data['pressure_mb'])
+    
+      print("Temperature: ", t)
+      print("Wind speed: ", ws)
+      print("Wind direction: ", wd)
+      print("Relative humidity: ", rh)
+      print("Pressure: ", p)
+        
+      temperature[i, j] = t
+      wind_speed[i, j] = ws
+      wind_direction[i, j] = wd
+      relative_humidity[i, j] = rh
+      pressure[i, j] = p
+      
+      time.sleep(1)
+      
+  return temperature, wind_speed, wind_direction, relative_humidity, pressure
+
+def interpolateScalar(data, kind='linear'):
+  x = np.linspace(0, 1, len(data))
+  xfine = np.linspace(0, 1, 100)
+  Z = None
+
+  if kind == "rbf":
+    xrbf, yrbf = np.meshgrid(x, x)
+    rbfi = interpolate.Rbf(xrbf.flatten(), yrbf.flatten(), 
+                           data.flatten(), epsilon=.25) 
+    X, Y = np.meshgrid(xfine, xfine)
+    Z = rbfi(X, Y)
+  else:
+    f = interpolate.interp2d(x, x, data, kind=kind)
+    Z = f(xfine, xfine)
+    
+  return Z
+  
+def plotScalar(temperature, cmap_):
+  #plt.contour(temperature, cmap=cm.jet)
+  #plt.imshow(temperature, cmap=cm.jet)
+  plt.pcolor(temperature, cmap=cmap_)
+  plt.show()
+  
+
+#M, N = 10, 10
+#temperature = getWeather(-35.3803, -72.3652, -35.5235, -72.1894, M, N)
+#_, _, wind_direction, relative_humidity, pressure = getWeather(-35.3803, -72.3652, -35.5235, -72.1894, M, N)
+#np.save('data/temperature.npy', temperature)
+#np.save('data/wind_speed.npy', wind_speed)
+#np.save('data/wind_direction.npy', wind_direction)
+#np.save('data/humidity.npy', relative_humidity)
+#np.save('data/pressure.npy', pressure)
+  
+temperature = np.load('data/temperature.npy')
+wind_speed = np.load('data/wind_speed.npy')
+wind_direction = np.load('data/wind_direction.npy')
+humidity = np.load('data/humidity.npy')
+pressure = np.load('data/pressure.npy')
+
+print(np.radians(wind_direction))
+
+T = interpolateScalar(temperature)
+WS = interpolateScalar(wind_speed)
+H = interpolateScalar(humidity)
+P = interpolateScalar(pressure, 'rbf')
+
+plotScalar(T, cm.jet)
+plotScalar(WS, cm.Blues)
+plotScalar(H, cm.GnBu)
+plotScalar(P, cm.Purples)
--- test.py
+++ test.py
@@ -1,20 +1,63 @@
 import firemodels.cellularautomata as ca
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
 
-def fireFocus(M, N, i, j, size):
-  focus = np.zeros((M, N))
-  focus[i-size:i+size, j-size:j+size] = np.ones((size, size)) 
-  return focus
+#def fireFocus(M, N, i, j, size):
+#  focus = np.zeros((M, N))
+#  focus[i-size:i+size, j-size:j+size] = np.ones((size, size)) 
+#  return focus
+#  
+## Testing
+#M = 101
+#N = 101
+#initial = fireFocus(M, N, 50, 50, 1)
+#rule = 1
+#neighborhood = 'vonneumann'#'moore'
+#times = 10
+#
+#ca = ca.new(M, N, initial, rule, neighborhood)
+#
+#sta = ca.propagate(times)
+base = "http://api.wunderground.com/api/35c89267da3bc202/conditions/q/"
+#base = "http://api.openweathermap.org/data/2.5/weather?"
+#lat=-38.7290843&lon=-72.6377406
+#end = "&units=metric&appid=bfba84a8e1deeb1bd7a3f034aaba4009"
+
+def getWeather(start_lat, start_lng, end_lat, end_lng, M, N):
+  delta_lat = (end_lat - start_lat) / M
+  delta_lng = (end_lng - start_lng) / N
   
-# Testing
-M = 101
-N = 101
-initial = fireFocus(M, N, 50, 50, 1)
-rule = 1
-neighborhood = 'vonneumann'#'moore'
-times = 10
+  temp = np.zeros((M, N))
+  
+  for i in range(M):
+    for j in range(N):
+      lat = start_lat + i * delta_lat
+      lng = start_lng + j * delta_lng
+      
+      #url = base + "lat=" + str(lat) + "&lon=" + str(lng) + end #+ ".json"      
+      url = base + str(lat) + "," + str(lng) + ".json" 
+      #data = requests.get(url).json()['main']
+      data = requests.get(url).json()['current_observation']
 
-ca = ca.new(M, N, initial, rule, neighborhood)
+      #d = float(data['temp'])
+      d = float(data['temp_c'])
+      #print(d)
+      
 
-sta = ca.propagate(times)
+      temp[i, j] = float(d)
+      
+  return temp
+
+M, N = 100, 100
+temperatura = getWeather(-35.3803, -72.3652, -35.5235, -72.1894, M, N)
+
+plt.imshow(temperatura)
+plt.show()
+#print(data['temp_c'])
+
+# Resampling image
+#subimg = np.zeros((SO_SIZE, SO_SIZE))
+#for i in range(SO_SIZE):
+#    for j in range(SO_SIZE):
+#        subimg[i, j] = img[5*i+3, 5*j+3]
