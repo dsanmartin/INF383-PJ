8aa2579aed1ed7431e6fefe22a1dc0c55bf7c846
xixolio <ignacio.araya.11@alumnos.usm.cl>
2018-05-06 16:01:35 -0300

    new discrete model with burning automata taken into account

---
+++ firemodels/discrete/temperature2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 15:15:59 2018
+
+@author: iaaraya
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors, cm
+#from matplotlib import cm
+
+class new:
+  temperatures = []
+  
+  def __init__(self, initial):
+    self.temperatures = [initial]
+
+  def propagate(self, timesteps,A):
+    for t in range(1, timesteps):
+      grid = self.temperatures[t-1]
+      #noise = np.random.normal(0, self.sigma, size=grid.shape)
+      east = np.roll(grid, 1, axis=0) 
+      west = np.roll(grid, -1, axis=0)
+      north = np.roll(grid, -1, axis=1)
+      south = np.roll(grid, 1, axis=1)
+      
+      temp = (1/5)*(grid + east + west + north + south) \
+              + A*(grid*((1000 - grid)/8000 + 4/5) + \
+            (east + west + north + south)*((1000 - grid)/2000 - 1/5))
+      
+      self.temperatures.append(temp)
+      
+      A[temp >= 600] = 1
+      A[temp < 600] = 0
+     
+      
+    return self.temperatures
+  
+  def plotTemperatures(self, t):
+    #cmap = colors.ListedColormap(['green', 'yellow'])
+    plt.imshow(self.temperatures[t], origin='lower', cmap=cm.jet)#, vmin=0)#, 
+               #vmin=0, vmax=np.max(np.array(self.temperatures)))
+    plt.colorbar()
+    plt.show()
+    
---
+++ test6.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 15:35:05 2018
+
+@author: iaaraya
+"""
+
+import firemodels.discrete.temperature2 as temperature
+import numpy as np
+
+def temperatureFocus(M, N):
+    temperature = np.zeros((M,N))
+    A = np.zeros((M,N))
+    A[M//2,N//2] = 1
+    A[M//2+1,N//2] = 1
+    temperature = temperature + A * 600
+    return temperature,A
+
+  
+M, N = 100, 100
+initial,A = temperatureFocus(M, N)
+
+times = 200
+
+dtemp = temperature.new(initial)
+states = dtemp.propagate(times,A)
+
+for i in range(len(states)):
+  dtemp.plotTemperatures(i)
